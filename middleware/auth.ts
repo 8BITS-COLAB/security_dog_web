338bb063aa80bf95fac0d616679cde211dd942d8
elioenaiferrari <elioenaiferrari@gmail.com>
2022-06-21 16:59:36 -0300

    verify if device is logged on refresh token

--- middleware/auth.ts
+++
@@ -1,9 +0,0 @@
-import { Context } from '@nuxt/types'
-
-export default ({ store, redirect }: Context) => {
-  const currentUser = store.getters['users/getCurrentUser']
-
-  if (currentUser?.id) {
-    return redirect('/auth/signin')
-  }
-}
